5f0e7232470265b29f8c8fe223f1c8ac62f25d83
miracleyoo <miracleyoo@163.com>
2022-06-12 00:53:01 -0400

    u

--- scripts/utils/shrink_net.py
+++ scripts/utils/shrink_net.py
@@ -5,7 +5,7 @@ import argparse
 import os.path as op
 
 sys.path.append('..')
-from scripts.model.unet_interface import ModelInteface
+from model.unet_interface import ModelInteface
 
 def get_model_path(date, root='/mnt/nfs/scratch1/zhongyangzha/DVS_HPE/dvs-hpe-light/lightning_logs', best=True):
     model_subdir = op.join(root, date, 'checkpoints')
@@ -20,7 +20,9 @@ def main(args):
     model_new_path = model_ori_path.replace('.ckpt', '.pt')
 
     model_ori = ModelInteface.load_from_checkpoint(model_ori_path)
+    print('[√] Original model loaded.')
     torch.save(model_ori.model, model_new_path)
+    print('[√] Done.')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
